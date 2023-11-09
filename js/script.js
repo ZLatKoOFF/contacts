67d1b12b8f1e1365cfe2217471556609f0bf1045
ZLatKo <140588476+ZLatKoOFF@users.noreply.github.com>
2023-11-09 22:15:54 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,36 +20,3 @@ item.style.backgroundImage = `url(${item.dataset.src})`;
 };
 });
 })();
-
-// Функция загрузки CSS или JS, править не нужно.
-function loadjscssfile(filename, filetype) {
-    if (filetype=="js") { //if filename is a external JavaScript file
-        var fileref=document.createElement('script')
-        fileref.setAttribute("type","text/javascript")
-        fileref.setAttribute("src", filename)
-    }
-    else if (filetype=="css") { //if filename is an external CSS file
-        var fileref=document.createElement("link")
-        fileref.setAttribute("rel", "stylesheet")
-        fileref.setAttribute("type", "text/css")
-        fileref.setAttribute("href", filename)
-    }
-    if (typeof fileref!="undefined")
-        document.getElementsByTagName("head")[0].appendChild(fileref)
-};
-
-// Вот тут нужно править под конкретные нужды.
-function appendAssets() {
-    if (window.innerWidth <= 1023) {
-        loadjscssfile("css/small.css", "css"); // передаем в функцию путь до файла и расширение
-    }
-    else if (window.innerWidth <= 1080 && window.innerWidth >= 768) {
-        loadjscssfile("css/styles.css", "css"); // передаем в функцию путь до файла и расширение
-    }
-    else if (window.innerWidth >= 1081) {
-        loadjscssfile("css/styles.css", "css"); // передаем в функцию путь до файла и расширение
-    }
-};
-
-// Инициализация конечной функции 
-appendAssets();
